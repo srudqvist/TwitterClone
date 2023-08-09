5622b87476881cb99bad4dece041c510b32ef2c3
Sam <60146956+srudqvist@users.noreply.github.com>
2023-08-08 21:42:33 -0500

    Added simple validation

--- twitter_clone/lib/main.dart
+++ twitter_clone/lib/main.dart
@@ -24,8 +24,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<FormState> _signInKey = GlobalKey();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,22 +34,38 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text("Twitter Clone"),
       ),
       body: Form(
+        key: _signInKey,
         child: Column(
           children: <Widget>[
             TextFormField(
               controller: _emailController,
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(hintText: "Enter your email address"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter an email";
+                }
+              },
             ), // email
             TextFormField(
               controller: _passwordController,
               obscureText: true,
               decoration: InputDecoration(hintText: "Enter a password"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter a password";
+                } else if (value.length < 12) {
+                  return "Password must be at least 12 characters";
+                }
+                return null;
+              },
             ), // password
             ElevatedButton(
                 onPressed: () {
-                  debugPrint("Email: ${_emailController.text}");
-                  debugPrint("Password: ${_passwordController.text}");
+                  if (_signInKey.currentState!.validate()) {
+                    debugPrint("Email: ${_emailController.text}");
+                    debugPrint("Password: ${_passwordController.text}");
+                  }
                 },
                 child: const Text("Submit"))
           ],
