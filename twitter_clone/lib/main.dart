0f49e11982bfab011302a03691874033d306cadd
Sam <60146956+srudqvist@users.noreply.github.com>
2023-08-08 21:32:13 -0500

    Basic login form

--- twitter_clone/lib/main.dart
+++ twitter_clone/lib/main.dart
@@ -1,18 +1,57 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Twitter Clone', // Set the title of your app
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Twitter Clone"),
+      ),
+      body: Form(
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(hintText: "Enter your email address"),
+            ), // email
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(hintText: "Enter a password"),
+            ), // password
+            ElevatedButton(
+                onPressed: () {
+                  debugPrint("Email: ${_emailController.text}");
+                  debugPrint("Password: ${_passwordController.text}");
+                },
+                child: const Text("Submit"))
+          ],
         ),
       ),
     );
