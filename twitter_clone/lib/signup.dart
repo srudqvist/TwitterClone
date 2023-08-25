7db39bfdf00742f8b314a24d58b2a91679297e5f
Sam <60146956+srudqvist@users.noreply.github.com>
2023-08-24 21:57:01 -0500

    added twitter logo using both image and font awesome

--- twitter_clone/lib/signup.dart
+++ twitter_clone/lib/signup.dart
@@ -1,31 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:twitter_clone/signup.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Twitter Clone', // Set the title of your app
-      home: MyHomePage(),
-    );
-  }
+  State<SignUp> createState() => _SignUpState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _SignUpState extends State<SignUp> {
   final GlobalKey<FormState> _signInKey = GlobalKey();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -40,20 +22,15 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const FaIcon(
-              FontAwesomeIcons.twitter,
-              color: Colors.blue,
-              size: 70,
+            const Image(
+              image: AssetImage('assets/twitter_blue.png'),
+              width: 100,
             ),
-            // const Image(
-            //   image: AssetImage('assets/twitter_blue.png'),
-            //   width: 100,
-            // ),
             const SizedBox(
               height: 20,
             ),
             const Text(
-              "Log in to Twitter",
+              "Sign up to Twitter",
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             Container(
@@ -116,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     }
                   },
                   child: const Text(
-                    "Log In",
+                    "Sign Up",
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 18,
@@ -125,10 +102,9 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             TextButton(
                 onPressed: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => const SignUp()));
+                  Navigator.of(context).pop();
                 },
-                child: const Text("Don't have an account? Sign up here")),
+                child: const Text("Already have an account? Log in here")),
           ],
         ),
       ),
